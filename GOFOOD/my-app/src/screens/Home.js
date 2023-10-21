29ea33390455a23e5a2d9af45e1aadb787e6f71b
ritu8081 <jadhavritu18@gmail.com>
2023-10-21 11:56:29 +0530

    complete 1

---
+++ GOFOOD/my-app/src/screens/Home.js
@@ -0,0 +1,18 @@
+// import './App.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+export default function App() {
+  return (
+    
+    <div>
+    <div> <Navbar/> </div>
+    <div>Body</div>
+    <div> <Footer/> </div>
+
+
+    
+
+     </div>
+  );
+}
+
